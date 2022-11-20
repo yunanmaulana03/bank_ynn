dc443f92291d9b38e9e3bcb770b754dd4bb1ea4d
yunanmaulana03 <yunan.maulana03@gmail.com>
2022-11-20 10:37:26 +0700

    add data plan form model, bloc, service

---
+++ lib/blocs/data_plan/data_plan_bloc.dart
@@ -0,0 +1,24 @@
+import 'package:bank_ynn/models/data_plan_form_model.dart';
+import 'package:bank_ynn/services/transaction_service.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'data_plan_event.dart';
+part 'data_plan_state.dart';
+
+class DataPlanBloc extends Bloc<DataPlanEvent, DataPlanState> {
+  DataPlanBloc() : super(DataPlanInitial()) {
+    on<DataPlanEvent>((event, emit) async {
+      if (event is DataPlanPost) {
+        try {
+          emit(DataPlanLoading());
+          await TranscationService().dataPlan(event.data);
+
+          emit(DataPlanSuccess());
+        } catch (e) {
+          emit(DataPlanFailed(e.toString()));
+        }
+      }
+    });
+  }
+}
--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:bank_ynn/models/data_plan_model.dart';
 import 'package:bank_ynn/models/top_up_model.dart';
 import 'package:bank_ynn/services/auth_service.dart';
 import 'package:bank_ynn/shared/shared_value.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/data_plan_form_model.dart';
 import '../models/transfer_form_model.dart';
 
 class TranscationService {
@@ -39,6 +41,28 @@ class TranscationService {
       final res = await http.post(
         Uri.parse(
           '$baseUrl/transfers',
+        ),
+        headers: {
+          'Authorization': token,
+        },
+        body: data.toJson(),
+      );
+
+      if (res.statusCode != 200) {
+        throw jsonDecode(res.body)['message'];
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> dataPlan(DataPlanFormModel data) async {
+    try {
+      final token = await AuthService().getToken();
+
+      final res = await http.post(
+        Uri.parse(
+          '$baseUrl/data_plans',
         ),
         headers: {
           'Authorization': token,
